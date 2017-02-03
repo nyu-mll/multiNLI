bf921f8250766e99dd4c1c1e5123f464e72dd49c
woollysocks <nikitanangia@gmail.com>
2017-02-03 17:58:01 -0500

    Incomplete run at data processing script

---
+++ data_processing.py
@@ -0,0 +1,92 @@
+
+import numpy as np
+
+import re
+import random
+import json
+import collections
+import numpy as np
+
+LABEL_MAP = {
+    "entailment": 0,
+    "neutral": 1,
+    "contradiction": 2
+}
+
+SEQ_LEN = 25
+
+PADDING = "<PAD>"
+UNKNOWN = "<UNK>"
+
+def load_nli_data(path):
+    data = []
+    with open(path) as f:
+        for line in f:
+            loaded_example = json.loads(line)
+            if loaded_example["gold_label"] not in LABEL_MAP:
+                continue
+            loaded_example["label"] = LABEL_MAP[loaded_example["gold_label"]]
+            data.append(loaded_example)
+        random.seed(1) ### Do we want to shuffle this data?
+        random.shuffle(data)
+    return data
+
+
+def sentences_to_padded_index_sequences(datasets):
+    '''Annotates datasets with feature vectors.'''
+    
+    # Extract vocabulary
+    def tokenize(string):
+        string = re.sub(r'\(|\)', '', string)
+        return string.lower().split()
+    
+    word_counter = collections.Counter()
+    for example in datasets[0]:
+        word_counter.update(tokenize(example['sentence1_binary_parse']))
+        word_counter.update(tokenize(example['sentence2_binary_parse']))
+        
+    vocabulary = set([word for word in word_counter])
+    vocabulary = list(vocabulary)
+    vocabulary = [UNKNOWN, PADDING] + vocabulary
+        
+    word_indices = dict(zip(vocabulary, range(len(vocabulary))))
+    indices_to_words = {v: k for k, v in word_indices.items()}
+        
+    for i, dataset in enumerate(datasets):
+        for example in dataset:
+            for sentence in ['sentence1_binary_parse', 'sentence2_binary_parse']:
+                example[sentence + '_index_sequence'] = np.zeros((SEQ_LEN), dtype=np.int32)
+
+                token_sequence = tokenize(example[sentence])
+                padding = SEQ_LEN - len(token_sequence)
+
+                for i in range(SEQ_LEN):
+                    if i >= padding:
+                        if token_sequence[i - padding] in word_indices:
+                            index = word_indices[token_sequence[i - padding]]
+                        else:
+                            index = word_indices[UNKNOWN]
+                    else:
+                        index = word_indices[PADDING]
+                    example[sentence + '_index_sequence'][i] = index
+    return indices_to_words, word_indices
+
+'''
+Load GloVe embeddings
+'''
+
+glove_home = '../../data/' ## edit to be in master
+words_to_load = 50000 ## edit to be variable, with ALL as an option
+
+def loadEmebdding(locaton)
+    with open(location) as f:
+        loaded_embeddings = np.zeros((len(word_indices), 50), dtype='float32')
+        for i, line in enumerate(f):
+            if i >= words_to_load: 
+                break
+            
+            s = line.split()
+            if s[0] in word_indices:
+                loaded_embeddings[word_indices[s[0]], :] = np.asarray(s[1:])
+
+     
