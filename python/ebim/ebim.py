d884419906bee5e65774653ee13ce695728182d7
woollysocks <nikitanangia@gmail.com>
2017-03-22 17:46:52 -0400

    simplify code

--- python/ebim/ebim.py
+++ python/ebim/ebim.py
@@ -28,7 +28,6 @@ class MyModel(object):
         ### Define biLSTM
         # Embedding lookup and dropout at embedding layer
         def emb_drop(x):
-            #emb = tf.nn.embedding_lookup(self.E, tf.transpose(x))
             emb = tf.nn.embedding_lookup(self.E, x)
             emb_drop = tf.nn.dropout(emb, self.keep_rate_ph)
             return emb_drop
@@ -73,12 +72,16 @@ class MyModel(object):
         scores_all = []
         premise_attn = []
         alphas = []
+
+        # can make loops over prem_seq and hyp_seq. premise_attn then is lenght of 
+        # prem_seq. make rnag(seq_lenght - prem_seq) prem_attn = prem_bi
         for i in range(self.sequence_length):
             scores_i_list = []
             for j in range(self.sequence_length):
                 score_ij = tf.reduce_sum(tf.multiply(premise_list[i], hypothesis_list[j]), 1, keep_dims=True)
                 scores_i_list.append(score_ij)
             scores_i = tf.stack(scores_i_list, axis=1)
+            # masked softmax?
             alpha_i = tf.nn.softmax(scores_i, dim=1)
             a_tilde_i = tf.reduce_sum(tf.multiply(alpha_i, hypothesis_bi), 1)
             premise_attn.append(a_tilde_i)
@@ -92,46 +95,48 @@ class MyModel(object):
         betas = []
         for j in range(self.sequence_length):
             scores_j = tf.unstack(scores_stack, axis=1)[j]
+            # masked softmax?
             beta_j = tf.nn.softmax(scores_j, dim=1)
             b_tilde_j = tf.reduce_sum(tf.multiply(beta_j, premise_bi), 1)
             hypothesis_attn.append(b_tilde_j)
 
             betas.append(beta_j)
 
+        # Make attention-weighted sentence representations into one tensor,
+        premimse_attns = tf.stack(premise_attn, axis=1)
+        hypothesis_attns = tf.stack(hypothesis_attn, axis=1)
+
         # For making attention plots, 
         self.alpha_s = tf.stack(alphas, axis=2)
         self.beta_s = tf.stack(betas, axis=2)
 
 
+
         ### Subcomponent Inference ###
 
-        m_a = []
-        m_b = []
-        
-        for i in range(self.sequence_length):
-            m_a_diff = premise_attn[i] - premise_list[i]
-            m_a_mul = premise_attn[i] * premise_list[i]
-            m_b_diff = hypothesis_attn[i] - hypothesis_list[i]
-            m_b_mul = hypothesis_attn[i] * hypothesis_list[i]
-            m_a_i = tf.concat([premise_list[i], premise_attn[i], m_a_diff, m_a_mul], 1)
-            m_b_i = tf.concat([hypothesis_list[i], hypothesis_attn[i], m_b_diff, m_b_mul], 1)
-            m_a.append(m_a_i)
-            m_b.append(m_b_i)
+        prem_diff = tf.subtract(premise_bi, premise_attns)
+        prem_mul = tf.multiply(premise_bi, premise_attns)
+        hyp_diff = tf.subtract(hypothesis_bi, hypothesis_attns)
+        hyp_mul = tf.multiply(hypothesis_bi, hypothesis_attns)
+
+        m_a = tf.concat([premise_bi, premise_attns, prem_diff, prem_mul], 2)
+        m_b = tf.concat([hypothesis_bi, hypothesis_attns, hyp_diff, hyp_mul], 2)
+
 
 
         ### Inference Composition ###
 
-        M_a = tf.stack(m_a, axis=1)
-        M_b = tf.stack(m_b, axis=1)
-
-        v1_outs, c3 = biLSTM(M_a, prem_seq_lengths, 'v1')
-        v2_outs, c4 = biLSTM(M_b, hyp_seq_lengths, 'v2')
+        v1_outs, c3 = biLSTM(m_a, prem_seq_lengths, 'v1')
+        v2_outs, c4 = biLSTM(m_b, hyp_seq_lengths, 'v2')
 
         v1_bi = tf.concat(v1_outs, axis=2)
         v2_bi = tf.concat(v2_outs, axis=2)
 
 
+
         ### Pooling Layer ###
+
+        # print self.v1_bi and v1_sum. over batch.
 
         v_1_ave = tf.reduce_sum(v1_bi, 1) / self.sequence_length
         v_2_ave = tf.reduce_sum(v2_bi, 1) / self.sequence_length
