7113809947ac2a7ceeb3bebfd50b72466d0bfecf
woollysocks <nikitanangia@gmail.com>
2017-03-25 19:11:06 -0400

    Fixed attention in bisltm+attn model

--- python/bilstm/bilstm_attn.py
+++ python/bilstm/bilstm_attn.py
@@ -33,11 +33,12 @@ class MyModel(object):
         def length(sentence):
             populated = tf.sign(tf.abs(sentence))
             length = tf.cast(tf.reduce_sum(populated, axis=1), tf.int32)
-            return length
+            mask = tf.cast(tf.expand_dims(populated, -1), tf.float32)
+            return length, mask
 
         # Get lengths of unpadded sentences
-        prem_seq_lengths = length(self.premise_x)
-        hyp_seq_lengths = length(self.hypothesis_x)
+        prem_seq_lengths, mask_prem = length(self.premise_x)
+        hyp_seq_lengths, mask_hyp = length(self.hypothesis_x)
 
         def biLSTM(inputs, seq_len, name):
             with tf.name_scope(name):
@@ -68,20 +69,22 @@ class MyModel(object):
 
         ### Attention ###
 
+        def masked_softmax(scores, mask):
+            numerator = tf.exp(tf.subtract(scores, tf.reduce_max(scores, 1, keep_dims=True))) * mask
+            denominator = tf.reduce_sum(numerator, 1, keep_dims=True)
+            weights = tf.div(numerator, denominator)
+            return weights
+
         scores_all = []
         premise_attn = []
         alphas = []
-
-        # can make loops over prem_seq and hyp_seq. premise_attn then is lenght of 
-        # prem_seq. make rnag(seq_lenght - prem_seq) prem_attn = prem_bi
         for i in range(self.sequence_length):
             scores_i_list = []
             for j in range(self.sequence_length):
                 score_ij = tf.reduce_sum(tf.multiply(premise_list[i], hypothesis_list[j]), 1, keep_dims=True)
                 scores_i_list.append(score_ij)
             scores_i = tf.stack(scores_i_list, axis=1)
-            # masked softmax?
-            alpha_i = tf.nn.softmax(scores_i, dim=1)
+            alpha_i = masked_softmax(scores_i, mask_hyp)
             a_tilde_i = tf.reduce_sum(tf.multiply(alpha_i, hypothesis_bi), 1)
             premise_attn.append(a_tilde_i)
             
@@ -89,26 +92,21 @@ class MyModel(object):
             alphas.append(alpha_i)
 
         scores_stack = tf.stack(scores_all, axis=2)
+        scores_list = tf.unstack(scores_stack, axis=1)
 
         hypothesis_attn = []
         betas = []
         for j in range(self.sequence_length):
-            scores_j = tf.unstack(scores_stack, axis=1)[j]
-            # masked softmax?
-            beta_j = tf.nn.softmax(scores_j, dim=1)
+            scores_j = scores_list[j] #tf.unstack(scores_stack, axis=1)[j]
+            beta_j = masked_softmax(scores_j, mask_prem)
             b_tilde_j = tf.reduce_sum(tf.multiply(beta_j, premise_bi), 1)
             hypothesis_attn.append(b_tilde_j)
 
             betas.append(beta_j)
 
-        # Make attention-weighted sentence representations into one tensor,
-        #premise_attns = tf.stack(premise_attn, axis=1)
-        #hypothesis_attns = tf.stack(hypothesis_attn, axis=1)
-
         # For making attention plots, 
         self.alpha_s = tf.stack(alphas, axis=2)
         self.beta_s = tf.stack(betas, axis=2)
-
 
 
         ### Mou et al. concat layer ###
@@ -116,9 +114,7 @@ class MyModel(object):
         diff = tf.subtract(premise_attn[-1], hypothesis_attn[-1])
         mul = tf.multiply(premise_attn[-1], hypothesis_attn[-1])
 
-        print diff
         h = tf.concat([premise_attn[-1], hypothesis_attn[-1], diff, mul], axis=1)
-        print h
 
         # MLP layer
         h_mlp = tf.nn.tanh(tf.matmul(h, self.W_mlp) + self.b_mlp)
